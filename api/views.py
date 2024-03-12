46b204fc2d8292e443be31acdee8e725bc34f57d
joltcops <pracheta@kgpian.iitkgp.ac.in>
2024-03-13 00:20:49 +0530

    Book backend updated

--- api/views.py
+++ api/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Book
+from .serializers import BookSerializer
 
 # Create your views here.
+
+@api_view(['GET'])
+def getBooks(request):
+    books=Book.objects.all()
+    bookSerializer=BookSerializer(books, many=True)
+    return Response(bookSerializer.data)
+@api_view(['GET'])
+def getBook(request, pk):
+    book = Book.objects.get(id = pk)
+    bookSerializer = BookSerializer(book, many=False)
+    return Response(bookSerializer.data)
+@api_view(['GET'])
+def getQuery(request, search):
+    # print(search)
+    books=Book.objects.all()
+    if search is not None:
+        books=books.filter(title__icontains=search)
+        bookSerializer=BookSerializer(books, many=True)
+    return Response(bookSerializer.data)
+@api_view(['GET'])
+def getCategory(request, pk):
+    # book = Book.objects.get(category = pk)
+    # bookSerializer = BookSerializer(book, many=False)
+    # return Response(bookSerializer.data)
+    books=Book.objects.all()
+    if pk is not None:
+        books=books.filter(category=pk)
+        bookSerializer=BookSerializer(books, many=True)
+    return Response(bookSerializer.data)
+@api_view(['GET'])
+def getAuthor(request, search):
+    # print(search)
+    books=Book.objects.all()
+    if search is not None:
+        books=books.filter(author__icontains=search)
+        bookSerializer=BookSerializer(books, many=True)
+    return Response(bookSerializer.data)
