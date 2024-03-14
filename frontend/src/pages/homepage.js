c7ea1ed59e9df9e2c18c5789c3ec2235bcb9d023
AnkitaMishra3003 <vijayalakshmiprakash6@kgpian.iitkgp.ac.in>
2024-03-14 11:25:39 +0530

    my commit

--- frontend/src/pages/homepage.js
+++ frontend/src/pages/homepage.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './homepage.css'; 
 
 function Home() {
 
     return (
-        <div>
-            <h1>Welcome to our website!</h1>
-            <div><Link to ='/search'>Search for Books</Link></div>
-            <div><Link to ='/login'>Login</Link></div>
+        <div className='div'>
+            <h1 className='a'>Welcome to our website!</h1>
+            <div className='link'><Link to ='/search'>Search for Books</Link></div>
+            <br/>
+            <div className='link'><Link to ='/login'>Login</Link></div>
         </div>
     );
 }
