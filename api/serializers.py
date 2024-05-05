850e44a61c16f07a6a31e1478420fb0f0a716174
joltcops <pracheta@kgpian.iitkgp.ac.in>
2024-05-05 21:37:44 +0530

    Not yet done!

--- api/serializers.py
+++ api/serializers.py
@@ -8,18 +8,3 @@ class BookSerializer(ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__' 
-
-class UserSerializer(ModelSerializer):
-    class Meta:
-        model=User
-        fields='__all__'
-
-class TransactionSerializer(ModelSerializer):
-    class Meta:
-        model=Transaction
-        fields='__all__'
-
-class ReqSerializer(ModelSerializer):
-    class Meta:
-        model=Req
-        fields='__all__'
